f9cf722c97f761d00ef37b777f586b38c9e3e91c
Andrey Butusov <agbutusov@mail.ru>
2024-02-18 22:00:22 +0300

    Add metod CrateMass

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿string[] CreateMass(Args)
-while 
+﻿Console.Clear();
+
+string[] CreateMass(int CountStr)
+{
+    string[] Array = new string[CountStr];
+    for (int i = 0; i < CountStr; i++)
+    {
+        Array[i] = Console.ReadLine()!;
+    }
+    return Array;
+}
+
+string[] str;
+Console.WriteLine("Введите количество элементов массива а затем задайте массив");
+int CountStr = int.Parse(Console.ReadLine()!);
+str = CreateMass(CountStr);
+Console.Write($"Массив [{string.Join(", ", str)}]");
