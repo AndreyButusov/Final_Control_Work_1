ce7255e7e0a7aab9af505d152a56341b18425213
Andrey Butusov <agbutusov@mail.ru>
2024-02-19 22:21:30 +0300

    Change metod CountSmallString

--- Program.cs
+++ Program.cs
@@ -10,24 +10,24 @@ string[] CreateMass(int CountStr)       //Создание массива дли
     return Array;
 }
 
-int CountSmallString(string[] str)      //Счетчик количества коротких значений в массиве
+int CountSmallString(string[] str, int Len)      //Счетчик количества коротких значений в массиве
 {
     int Count = 0;
     for (int i = 0; i < str.Length; i++)
     {
-        if (str[i].Length <= 3)
+        if (str[i].Length <= Len)
             Count++;
     }
     return Count;
 }
 
-string[] ChangeMass(string[] str)       //Метод создания нового массива с элементами, длина которых менее 3 символов
+string[] ChangeMass(string[] str, int Len)       //Метод создания нового массива с элементами, длина которых менее "Len" символов
 {
-    string[] newstr = new string[CountSmallString(str)];
+    string[] newstr = new string[CountSmallString(str, Len)];
     int j = 0;
     for (int i = 0; i < str.Length; i++)
     {
-        if (str[i].Length <= 3)
+        if (str[i].Length <= Len)
         {
             newstr[j] = str[i];
             j++;
@@ -41,5 +41,5 @@ Console.WriteLine("Введите количество элементов мас
 int CountStr = int.Parse(Console.ReadLine()!);
 str = CreateMass(CountStr);
 Console.WriteLine($"Массив [{string.Join(", ", str)}]");
-Console.Write("Массив коротких строковых значений:");
-Console.Write($" [{string.Join(", ", ChangeMass(str))}]");
+Console.Write($"Массив строковых значений менее 3 символов:");
+Console.Write($" [{string.Join(", ", ChangeMass(str, 3))}]");
